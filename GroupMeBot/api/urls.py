4941351ca210a0a7bb12ca0573b64770696b0319
Yonathan Fisseha <yonathan.fisseha@gmail.com>
2018-03-24 18:33:59 -0400

    updated broken links

--- GroupMeBot/api/urls.py
+++ GroupMeBot/api/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.conf.urls import include, url
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('', views.schedule, name="schedule"),
-    path('', views.memes, name="memes"),
+    url(r'^', views.index, name="index"),
+    url(r'^schedule', views.schedule, name="schedule"),
+    url(r'^memes', views.memes, name="memes"),
 ]
